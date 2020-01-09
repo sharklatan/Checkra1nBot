d4f5dd754e570836a88e2eb884924a4fe559ac45
Raphael Tang <raphpb1912@gmail.com>
2020-01-09 17:12:16 +0800

    Optimised imports 🔧

--- src/main/java/bot/Actions.java
+++ src/main/java/bot/Actions.java
@@ -1,8 +1,6 @@
 package bot;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
